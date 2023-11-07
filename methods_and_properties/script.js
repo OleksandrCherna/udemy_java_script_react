dddcf02366a8d7ac6cb205845aade7f8e3fb84d0
Oleksandr Cherna <grek976@gmail.com>
2023-11-07 12:56:22 +0100

    28. Методы и свойства строк и чисел

---
+++ methods_and_properties/script.js
@@ -0,0 +1,26 @@
+"use strict";
+
+const str = "test";
+//const arr = [1, 2, 3];
+
+console.log(str.toUpperCase());
+
+
+const fruit = "Some fruit";
+
+console.log(fruit.indexOf("fruit"));
+console.log(fruit.indexOf("q"));
+
+const logg = "Hello world!";
+
+console.log(logg.slice(-6, -1));
+console.log(logg.substring(6, 10));
+console.log(logg.substr(6, 10));
+
+const num = 12.2;
+console.log(Math.round(num));
+
+const test = "12.2px";
+console.log(parseInt(test));
+console.log(parseFloat(test));
+
