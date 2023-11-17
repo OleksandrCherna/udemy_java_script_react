4c00d4b2a2c61f4437df2af508e472b3673ec131
Oleksandr Cherna <grek976@gmail.com>
2023-11-17 10:40:15 +0100

    32. Объекты, деструктуризация объектов (ES6)

--- objects/script.js
+++ objects/script.js
@@ -8,20 +8,45 @@ const options = {
 	width: 1024,
 	heigth: 1024,
 	colors: {
-		border: "bllack",
+		border: "black",
 		bg: "red"
+	},
+	//создание метода внутри обьекта
+	makeTest: function() {
+		console.log("test")
 	}
 };
 
 //получение значения по ключу
-console.log(options.name); 
+//console.log(options.name); 
 
 //удаление свойства из обьекта
 //delete options.name; 
 
-console.log(options);
-
+//console.log(options);
+//let counter = 0;
 //перебор ключей обьекта с их значением
 for (let key in options) {
-	console.log(`Свойство ${key} имеет значение ${options[key]}`);
+	//перебор обьектов внутри обьекта
+	if (typeof(options[key]) === "object") {
+		for( let i in options[key])	{
+			console.log(`Свойство ${i} имеет значение ${options[key][i]}`);
+			//подсчет кол-ва свойств в обьекте !не совсем корректный метод!
+			//counter++;	
+		}			
+	} else {
+		console.log(`Свойство ${key} имеет значение ${options[key]}`);
+		//counter++;
+	}	
 }
+
+//console.log(counter);
+//метод для посчета количества ключей в обьекте
+console.log(Object.keys(options).length);
+//вызов метода внутри обекта
+options.makeTest();
+
+//деструктуризация обьекта
+
+const {border, bg} = options.colors;
+console.log(border);
